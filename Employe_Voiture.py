ef8e281fb718783e44b29925a015f3333ba3c78b
Hafsi <300151732@monboreal.ca>
2026-03-29 15:10:40 -0400

    Creation de la classe Employe

---
+++ Employe_Voiture.py
@@ -0,0 +1,7 @@
+class Employe:
+    def __init__(self,num_permis,nom,prenom):
+        self.num_permis=num_permis
+        self.nom=nom
+        self.prenom=prenom
+        self.voiture_service= None
+
